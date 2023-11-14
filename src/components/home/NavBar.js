6bc559e3704dd05fd995e2e83534e46b8525e8a4
Muchael123 <mainamichael2849@gmail.com>
2023-11-14 15:59:29 +0300

    first commit

---
+++ src/components/home/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import nav from './images/homeNav.gif'
+
+function NavBar() {
+  return (
+    <div>
+        {/* Upper .gif file */}
+        <div>
+            <image href={nav} alt='img' />
+        </div>
+        {/* medium nav with links */}
+        <div>
+
+        </div>
+        {/* lower navs */}
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
